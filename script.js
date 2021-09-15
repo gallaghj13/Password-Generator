798fa187162f8749f07dae82de4965278046d495
John Gallagher <gallagherj0913@gmail.com>
2021-09-15 12:21:36 -0600

    Updated javascript with formatting and comments

--- script.js
+++ script.js
@@ -1,129 +1,104 @@
 // Assignment Code
 generateBtn = document.querySelector("#generate");
 
-// Global ables
-// lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l", "m",
-//                       "n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-// uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
-//                     ,"N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-// numberString = "0123456789";
-// numberArray = numberString.split("");
-
-// specialCharString = "!@#$%^&*()?";
-// specialCharArray = specialCharString.split("");
-
-
-
-
 // Write password to the #password input
 function writePassword() {
-    
-    passLength = window.prompt("How many characters long do you want the password to be? (Must be 8-128 long)");
-        if (passLength < 8) {
-            window.alert("The password must contain at least 8 characters and no more than 128.")
-            return;
-        }
-        if (passLength > 128) {
-            window.alert("The password must contain at least 8 characters and no more than 128.")
-            return;
-        };
-    lowerAns = window.confirm("Do you want lowercase letters in your password?");
-    upperAns = window.confirm("Do you want uppercase letters in your password?");
-    numberAns = window.confirm("Do you want numbers in your password?");
-    specialAns = window.confirm("Do you want special characters in your password?");
-    
+// Starts with questions that will be the conditions for the password
+  passLength = window.prompt("How many characters long do you want the password to be? (Must be 8-128 long)");
 
+  if (passLength < 8 || passLength > 128) {
+    window.alert(
+      "The password must contain at least 8 characters and no more than 128."
+    );
+    return;
+  }
 
+  lowerAns = window.confirm("Do you want lowercase letters in your password?");
+  upperAns = window.confirm("Do you want uppercase letters in your password?");
+  numberAns = window.confirm("Do you want numbers in your password?");
+  specialAns = window.confirm("Do you want special characters in your password?");
 
-password = generatePassword();
-function generatePassword() {
-password = [];
-lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l", "m",
-                      "n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  password = generatePassword();
 
-uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
-                    ,"N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  function generatePassword() {
+// Created an empty array for the password and created arrays for each character type
+    password = [];
+    lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+    lowercaseArray = lowerCaseString.split("");
 
-numberString = "0123456789";
-numberArray = numberString.split("");
+    upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    uppercaseArray = upperCaseString.split("");
 
-specialCharString = "!@#$%^&*()?";
-specialCharArray = specialCharString.split("");
+    numberString = "0123456789";
+    numberArray = numberString.split("");
 
+    specialCharString = "!@#$%^&*()?";
+    specialCharArray = specialCharString.split("");
+// Used a while loop to run through the answers to the prompts and pick characters accordingly
+    while (password.length < passLength) {
+      if (lowerAns === true && password.length < passLength) {
+        password.push(
+          lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]
+        );
+      }
 
-   
-// for (var i=0; i < passLength; i++) {
-// let n = 0
+      if (upperAns === true && password.length < passLength) {
+        password.push(
+          uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]
+        );
+      }
 
-while (password.length < passLength) {
-    if (lowerAns === true && password.length < passLength) {
-        password.push(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]);
-    }
+      if (numberAns === true && password.length < passLength) {
+        password.push(
+          numberArray[Math.floor(Math.random() * numberArray.length)]
+        );
+      }
 
-    if (upperAns === true && password.length < passLength) {
-        password.push(uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]);
-    }
+      if (specialAns === true && password.length < passLength) {
+        password.push(
+          specialCharArray[Math.floor(Math.random() * specialCharArray.length)]
+        );
+      }
 
-    if (numberAns === true && password.length < passLength) {
-        password.push(numberArray[Math.floor(Math.random() * numberArray.length)]);
-    }
-
-    if (specialAns === true && password.length < passLength) {
-        password.push(specialCharArray[Math.floor(Math.random() * specialCharArray.length)]);
-    }
-
-    if (password.length === passLength && lowerAns === true) {
-        password.push(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]);
+      if (password.length === passLength && lowerAns === true) {
+        password.push(
+          lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]
+        );
         return password;
-    }
-    if (password.length === passLength && upperAns === true) {
-        password.push(uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]);
+      }
+      if (password.length === passLength && upperAns === true) {
+        password.push(
+          uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]
+        );
         return password;
-    }
-    if (password.length === passLength && numberAns === true) {
-        password.push(numberArray[Math.floor(Math.random() * numberArray.length)]);
+      }
+      if (password.length === passLength && numberAns === true) {
+        password.push(
+          numberArray[Math.floor(Math.random() * numberArray.length)]
+        );
         return password;
-    }
-    if (password.length === passLength && specialAns === true) {
-        password.push(specialCharArray[Math.floor(Math.random() * specialCharArray.length)]);
+      }
+      if (password.length === passLength && specialAns === true) {
+        password.push(
+          specialCharArray[Math.floor(Math.random() * specialCharArray.length)]
+        );
         return password;
+      }
     }
-   
+// After the password is generated through the loop, it gets shuffled or randomized
+    var shuffledPassword = function (passwordArray) {
+      passwordArray.sort(() => Math.random() - 0.5);
+      return passwordArray;
+    };
+// Randomized password array is then made into a string to be displayed on the webpage
+    shuffledPassword(password);
+    return password.join("");
+  }
 
-    // return password.join('');
-}
-var shuffledPassword = function(inputArray) {
-     inputArray.sort(()=> Math.random() - 0.5);
-     return inputArray;
-};
-shuffledPassword(password);
-return password.join('');
-
-
-};
-
-
- 
-
-passwordText = document.querySelector("#password");
+  passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-    
-
-
-
-
-
-
-
-
-
